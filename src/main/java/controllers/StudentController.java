7b7a30a19825fa98931e5174451c7d6d03b3e8c8
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-20 14:59:58 +0000

    Students showing - but lessons arent

---
+++ src/main/java/controllers/StudentController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import db.DBHelper;
+import db.Seeds;
+import models.Student;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
+
+public class StudentController {
+    public static void main(String[] args) {
+        staticFileLocation("public");
+//        ManagersController managersController = new ManagersController();
+        Seeds.seedData();
+
+        get("/students", (req, res)-> {
+            Map<String, Object> model = new HashMap();
+            List<Student> students = DBHelper.getAll(Student.class);
+            model.put("template", "templates/students/index.vtl");
+            model.put("students", students);
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+    }
+}
